3eea423f125e8ae0a5acd929985c80c8c870b6b2
sammunno <sammunno12@icloud.com>
2017-03-28 17:35:52 -0400

    Update Calculator.java
    added square method

--- CPS491Calculator/src/cps491calculator/Calculator.java
+++ CPS491Calculator/src/cps491calculator/Calculator.java
@@ -17,6 +17,9 @@ public class Calculator {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    public int square(int num){
+           return num*num;
+    }
 
 
 }
