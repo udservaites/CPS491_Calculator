76bb8406b4cb8a3f72728b5998435df6224fab15
dsamuels1 <dsamuels1@udayton.edu>
2017-03-28 17:53:25 -0400

    Update Calculator.java
    added subtraction

--- CPS491Calculator/src/cps491calculator/Calculator.java
+++ CPS491Calculator/src/cps491calculator/Calculator.java
@@ -29,6 +29,10 @@ public class Calculator {
     public double addition(double add1, double add2) {
         return add1 + add2;
     }
+     
+    public double subtraction(double sub1, double sub2) {
+        return sub1 - sub2;
+    }
 
     public static double divide(double numerator, double denominator){
         if(denominator == 0){
